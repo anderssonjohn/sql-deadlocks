f8214b03c8a8a18528a91db719701239e9e5db0b
John Andersson <hello@johnandersson.me>
2023-10-27 13:56:10 +0200

    Add example of dirty read

---
+++ dirty-read.sql
@@ -0,0 +1,33 @@
+set transaction isolation level read uncommitted
+begin transaction
+
+select *
+into #TempUsers
+FROM Users
+
+update Users
+set OrganizationId = #TempUsers.OrganizationId,
+    Name           = 'John 1234'
+from #TempUsers
+where Users.Name = 'John' and #TempUsers.Name = 'John'
+
+commit
+
+select *
+from Users
+where Name = 'John 1234'
+
+
+-- run below in another session
+
+begin transaction;
+
+update Users
+set OrganizationId = 2
+where name = 'John'
+
+rollback
+
+select *
+from Users
+where Name = 'John'
